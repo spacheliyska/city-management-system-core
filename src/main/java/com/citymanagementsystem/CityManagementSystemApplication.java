e2108944bf642ce9a6be898767a0fde1d7d40ef4
spacheliyska <simonapacheliyska@gmail.com>
2023-11-14 10:33:16 +0200

    Add initial Spring Boot project

---
+++ src/main/java/com/citymanagementsystem/CityManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.citymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citymanagementsystem/CityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
