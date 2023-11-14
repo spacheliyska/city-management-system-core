3b184931714b3d125585bdb2346bf52de831b417
spacheliyska <simonapacheliyska@gmail.com>
2023-11-14 12:37:09 +0200

    Inject City Service in City Controller

--- src/main/java/com/citymanagementsystem/city/CityController.java
+++ src/main/java/com/citymanagementsystem/city/CityController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CityController {
     private CityService cityService;
 
+    public CityController(CityService cityService) {
+        this.cityService = cityService;
+    }
+
     @GetMapping("/cities")
     public Iterable<City> list() {
         return this.cityService.list();
