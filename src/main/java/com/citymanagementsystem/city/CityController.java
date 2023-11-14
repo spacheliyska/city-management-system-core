4dd3502880766013fc52a6f60af1ba9d3b174235
spacheliyska <simonapacheliyska@gmail.com>
2023-11-14 18:01:23 +0200

    Enable CORS

--- src/main/java/com/citymanagementsystem/city/CityController.java
+++ src/main/java/com/citymanagementsystem/city/CityController.java
@@ -1,11 +1,9 @@
 package com.citymanagementsystem.city;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class CityController {
     private CityService cityService;
 
